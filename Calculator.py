b3e425839fe752b5e521eac50c187bd330058973
Aniket01-Bahman <ss13133319@gmail.com>
2025-07-26 20:14:19 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: " ))
+add=a+b
+sub=a-b
+multiply=a*b
+divide=a/b
+print("\nAddition: ",add ,"\nSubtraction: ", sub,"\nMultiplication: ",multiply,"\nDivision: ",divide)
---
+++ welcome full name.py
@@ -0,0 +1,4 @@
+a=str(input("Enter your first name: "))
+b=str(input("Enter your last name: "))
+full_name = a+ " " + b
+print(f"Hello, {full_name}! Welcome to the Python program.")
